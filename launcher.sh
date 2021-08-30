39f5048de5609aa88b0bc2ddfb03725272125daa
olivier st-laurent <kzaurckichz@gmail.com>
2021-08-29 23:47:23 -0400

    'Published new release build'

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,5 @@ cd `dirname $0` &&\
 git config core.filemode false &&\
 git lfs install &&\
 (git pull --ff-only origin master || (git reset --hard && git pull -f --rebase --allow-unrelated-histories origin master && git reset --hard && git clean -df)) &&\
+chmod +x ./g4d &&\
 ./g4d
