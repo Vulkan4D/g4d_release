617debb725850ae31389bf89b22c56fb9417546b
olivier st-laurent <kzaurckichz@gmail.com>
2021-10-16 16:27:25 -0400

    ..

--- launcher_debug_client.sh
+++ launcher_debug_client.sh
@@ -3,4 +3,5 @@ git config core.filemode false &&\
 git lfs install &&\
 git fetch && git checkout debug && (git pull --ff-only origin debug || (git reset --hard && git pull -f --rebase --allow-unrelated-histories origin debug && git reset --hard && git clean -df)) &&\
 chmod 777 g4d &&\
+rm -rf client clients server servers
 ./g4d -client
